1052272fe24accd013012f13b653f59e418016dc
Vitaliy Derda <derdavitaliy@gmail.com>
2020-08-08 23:32:15 +0300

    Updates

---
+++ precache-manifest.758170f3b5a64d6ea690fe1ca92ebcf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a8b9ca2569b8972c8315337f2dd975",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "43da29bfa19e54517e27",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.baa18f7a.chunk.css"
+  },
+  {
+    "revision": "da1d0ea102951989c5fd",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.9941e8a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.9941e8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43da29bfa19e54517e27",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.f9833c89.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
